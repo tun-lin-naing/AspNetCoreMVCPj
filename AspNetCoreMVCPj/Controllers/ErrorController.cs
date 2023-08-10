6e3fa188145d90670f7a2ee791dc5edeb0563bdd
Tun Lin Naing <mctunlynn@gmail.com>
2023-08-10 15:27:10 +0630

    Add Not Found 404

---
+++ AspNetCoreMVCPj/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMVCPj.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch(statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, this page could not be found!";
+                    break;
+            }
+
+            return View("NotFound");
+        }
+    }
+}

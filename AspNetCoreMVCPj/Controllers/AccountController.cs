dbf307cb4f1dd72634d9fb2f50aca6617c2e2123
Tun Lin Naing <mctunlynn@gmail.com>
2023-08-10 17:14:00 +0630

    Add Register form

---
+++ AspNetCoreMVCPj/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMVCPj.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
